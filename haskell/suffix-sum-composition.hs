aed51cd0edcdb19332e8908175b02d94c1abafd5
Aditya <adityam.rtm@gmail.com>
2020-08-15 20:22:33 +0530

    Update suffix-sum-composition.hs

--- haskell/suffix-sum-composition.hs
+++ haskell/suffix-sum-composition.hs
@@ -1,5 +1,10 @@
+suffixSum :: [Integer] -> [Integer]
+suffixSum (x:[]) = [x,0]
+suffixSum (x:ls) = (x+h:h:ss) where (h:ss) = suffixSum ls
+
 suffixSumUsingApplicatives :: [Integer] -> [Integer]
 suffixSumUsingApplicatives = foldr (curry $ (:) <$> uncurry (flip $ (+) . head) <*> snd) [0]
 
 suffixSumUsingMonads :: [Integer] -> [Integer]
 suffixSumUsingMonads = foldr ((>>= (:)) .  (flip $ (+) . head)) [0]
+

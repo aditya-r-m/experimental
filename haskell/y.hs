d56486abd6dcd300a35ee15445b4ed64d60c3bbc
Aditya Mishra <adityam.rtm@gmail.com>
2020-08-30 19:02:32 +0530

    add a few more y-combinator implementations

--- haskell/y.hs
+++ haskell/y.hs
@@ -12,4 +12,17 @@ partialFactorial self x = x * self (x - 1)
 factorial :: Int -> Int
 factorial = y partialFactorial
 
-main = print $ factorial 9
+f9 :: Int
+f9 = factorial 9
+
+-- recursion without assigning names to functions
+f9' :: Int
+f9' = (\f -> (\x -> f (uc x x)) (\x -> f (uc x x)))
+  (\slf -> \x -> if x == 0 then 1 else x * slf (x - 1)) 9
+
+-- compact version
+f9'' :: Int
+f9'' = (\f -> (\x -> uc x x) (\x -> f (uc x x)))
+  (\slf -> \x -> if x == 0 then 1 else x * slf (x - 1)) 9
+
+main = print $ [f9, f9', f9'']

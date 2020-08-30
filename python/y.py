f2dd969bf32dd4f0f7cf0395f00aadb447f95f82
Aditya Mishra <adityam.rtm@gmail.com>
2020-08-30 19:02:32 +0530

    add a few more y-combinator implementations

--- python/y.py
+++ python/y.py
@@ -6,3 +6,17 @@ partialFactorial = lambda slf: lambda a: a * slf(a - 1) if a else 1
 factorial = y(partialFactorial)
 
 print(factorial(9))
+
+# recursion without assigning names to functions
+print(
+  (lambda f: (lambda x: lambda a: f(x(x))(a)) (lambda x: lambda a: f(x(x))(a)))
+  (lambda slf: lambda a: a * slf(a - 1) if a else 1)
+  (9)
+)
+
+# compact version
+print(
+  (lambda f: (lambda x: x(x)) (lambda x: lambda a: f(x(x))(a)))
+  (lambda slf: lambda a: a * slf(a - 1) if a else 1)
+  (9)
+)

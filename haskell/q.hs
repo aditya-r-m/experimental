445323007245e233616b174e7262fdaa3828adef
Aditya Mishra <adityam.rtm@gmail.com>
2020-10-01 18:55:44 +0530

    clean up some haskell implementations

--- haskell/q.hs
+++ haskell/q.hs
@@ -1,3 +1,13 @@
+type Q = ([Int], [Int])
+
+push :: Int -> Q -> Q
+push x (l, r) = (x:l, r)
+
+pop :: Q -> Maybe (Q, Int)
+pop ([], []) = Nothing
+pop (l, []) = pop ([], reverse l)
+pop (l, x:xs) = Just ((l, xs), x)
+
 
 type Deque = ([Int], Int, [Int], Int)
 

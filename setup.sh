04d57bfd9f0adc7cb42a3450b829c636b5fb8d1a
Aditya Mishra <adityam.rtm@gmail.com>
2021-10-11 12:39:36 +0530

    Create setup script

---
+++ setup.sh
@@ -0,0 +1,25 @@
+sudo apt update
+sudo apt install vim screen htop git golang haskell-platform firefox curl zsh
+
+echo "
+caption string \"%{03} \"
+rendition so =00
+startup_message off
+
+focus
+screen -t \"zsh\" zsh
+split -v
+focus
+screen -t \"htop\" htop
+focus
+" > ~/.screenrc
+
+echo "
+set showtabline=0
+set tabstop=4
+set shiftwidth=4
+set softtabstop=4
+set expandtab smarttab
+
+autocmd BufWritePre * :%s/\s\+$//e
+" > ~/.vimrc

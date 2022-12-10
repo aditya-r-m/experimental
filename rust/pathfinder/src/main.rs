27f9bb1b7ae6ee5e57a498c1a319d836d1a1fb50
Aditya Mishra <adityam.rtm@gmail.com>
2022-12-10 14:47:07 +0530

    implement basic dijkstra's algorithm for benchmarking

--- rust/pathfinder/src/main.rs
+++ rust/pathfinder/src/main.rs
@@ -1,80 +1,72 @@
+use std::collections::BinaryHeap;
 use std::io;
 
-fn read_usize_pair() -> (usize, usize) {
+fn read_u64_vec() -> Vec<u64> {
+    let mut result: Vec<u64> = Vec::new();
     let mut input = String::new();
     io::stdin()
         .read_line(&mut input)
         .expect("Failed to read line");
-    let input_vec = input.split_whitespace().collect::<Vec<&str>>();
-    let mut u: usize = 0;
-    let mut v: usize = 0;
-    match input_vec[0].parse::<usize>() {
-        Ok(i) => u = i,
-        Err(..) => println!("failed to parse as usize {}", input_vec[0]),
-    };
-    match input_vec[1].parse::<usize>() {
-        Ok(i) => v = i,
-        Err(..) => println!("failed to parse as usize {}", input_vec[1]),
-    };
-    (u, v)
+    for val in input.split_whitespace() {
+        match val.parse::<u64>() {
+            Ok(i) => result.push(i),
+            Err(..) => println!("failed to parse as u64 {}", val),
+        };
+    }
+
+    result
 }
 
-fn find_distance(graph: &Vec<Vec<usize>>, s: usize, t: usize) -> usize {
+fn find_distance(graph: &Vec<Vec<(usize, u64)>>, s: usize, t: usize) -> Option<u64> {
     let n = graph.len();
     if s >= n || t >= n {
-        return usize::MAX;
+        return None;
     }
-    let mut vecq: Vec<usize> = vec![s, n];
+    let mut q = BinaryHeap::new();
     let mut visited: Vec<bool> = vec![false; n];
+    q.push((0, s));
     visited[s] = true;
-    let mut i: usize = 0;
-    let mut d: usize = 0;
-    while i < vecq.len() {
-        let u = vecq[i];
-        i += 1;
-        if u == n {
-            if i == vecq.len() {
-                return usize::MAX;
-            }
-            d += 1;
-            vecq.push(n);
-            continue;
-        }
+    while let Some((d, u)) = q.pop() {
         if u == t {
-            return d;
+            return Some(d);
         }
-        for &v in graph[u].iter() {
+        for &(v, w) in graph[u].iter() {
             if visited[v] {
                 continue;
             }
-            vecq.push(v);
+            q.push((d + w, v));
             visited[v] = true;
         }
     }
-    usize::MAX
+
+    None
 }
 
 fn main() {
     println!("enter the count of vertices & edges");
-    let (n, m) = read_usize_pair();
+    let mut input_vec = read_u64_vec();
+    let (n, m) = (input_vec[0] as usize, input_vec[1] as usize);
     println!("{:?}", (n, m));
-    let mut graph: Vec<Vec<usize>> = Vec::new();
+    let mut graph: Vec<Vec<(usize, u64)>> = Vec::new();
     graph.resize_with(n, Vec::new);
     for i in 0..m {
-        println!("enter (directional) edge {}", i);
-        let (u, v) = read_usize_pair();
-        graph[u].push(v);
+        println!("enter edge {}", i);
+        input_vec = read_u64_vec();
+        let (u, v, w) = (input_vec[0] as usize, input_vec[1] as usize, input_vec[2]);
+        graph[u].push((v, w));
+        graph[v].push((u, w));
     }
     println!("The adjacency list representation of the graph is as follows");
     println!("{:?}", graph);
     loop {
         println!("Enter the source & target, Enter 0 0 to terminate");
-        let (s, t) = read_usize_pair();
+        input_vec = read_u64_vec();
+        let (s, t) = (input_vec[0] as usize, input_vec[1] as usize);
         if s == 0 && t == 0 {
             break;
         }
         println!(
-            "The distance from source to target is {}",
+            "The distance from source to target is {:?}",
             find_distance(&graph, s, t)
         );
     }

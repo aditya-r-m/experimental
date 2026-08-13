abe671ba4eceecef1c1fed4c53f1473138e93b39
Aditya <adityam.rtm@gmail.com>
2026-08-13 08:40:00 +0000

    wip

--- linear_algebraic_computations.py
+++ linear_algebraic_computations.py
@@ -378,13 +378,18 @@ class Projection(Scene):
                 ))
             self.play(Create(node_text), *(Create(arrow) for arrow in arrows))
         self.wait(8)
-        return
+        self.play(FadeOut(*self.mobjects))
 
+class S(Scene):
+    def construct(self):
+        self.camera.background_color = "#eee8d5"
         # 1. Projection covectors : Derivation from single axis measurement and rotation covector
-        axes = Axes(x_length=6, y_length=6, x_range=[-4,4,2], y_range=[-4,4,2], axis_config={"color": BLACK})
-        axes.move_to(RIGHT*3)
-        self.play(Create(axes))
-        base_arrow = Arrow(color=CS[0]).put_start_and_end_on(axes.c2p(0, 0), axes.c2p(1, 1))
+        grid = NumberPlane(
+            x_range=(-4, 4, 1),
+            axis_config={"color":BLACK}
+        ).move_to(RIGHT*3)
+        self.play(Create(grid))
+        base_arrow = Arrow(color=CS[0]).put_start_and_end_on(grid.c2p(0, 0), grid.c2p(1, 1))
         self.play(Create(base_arrow))
         self.wait(8)
         return

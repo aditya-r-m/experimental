24755535bfdfc2986843344dd63603a9cf16fdd9
Aditya <adityam.rtm@gmail.com>
2026-08-19 16:25:55 +0000

    wip

--- linear_algebraic_computations.py
+++ linear_algebraic_computations.py
@@ -314,9 +314,10 @@ class Determinant(Scene):
         self.play(FadeOut(tex))
 
 
-class Projection(Scene):
+class S(Scene):
    def construct(self):
-        self.camera.background_color = "#eee8d5"
+        Text.set_default(color=LIGHT_GRAY, font_size=24)
+        MathTex.set_default(font_size=42)
         title_texts = [
             "Rotation Matrix",
             "Projection Vector",
@@ -328,7 +329,7 @@ class Projection(Scene):
             "Projection Matrix",
             "OLS Linear Regression",
         ]
-        node_texts = list(map(lambda t: Text(t, font="Consolas", font_size=16, color=BLACK), title_texts))
+        node_texts = list(map(lambda t: Text(t, color=WHITE, font="Consolas", font_size=16), title_texts))
         layout = [
             np.array([-3,3,0]),
             np.array([3,3,0]),
@@ -373,17 +374,10 @@ class Projection(Scene):
                     tip_length=0.1,
                     max_stroke_width_to_length_ratio=100,
                     max_tip_length_to_length_ratio=100,
-                    color=BLACK,
                 ))
             self.play(Create(node_text), *(Create(arrow) for arrow in arrows))
         self.wait(8)
         self.play(FadeOut(*self.mobjects))
-
-class S(Scene):
-    def construct(self):
-        self.camera.background_color = "#eee8d5"
-        Text.set_default(color=BLACK, font_size=24)
-        MathTex.set_default(color=BLACK, font_size=42)
         def update_title(title, content):
             updated_title = Text(content).to_edge(UP+LEFT)
             self.play(ReplacementTransform(title, updated_title))
@@ -391,13 +385,13 @@ class S(Scene):
         title = Text("Constrained Optimization").to_edge(UP+LEFT)
         grid = NumberPlane(
             x_range=(-4,4,1),
-            axis_config={"color":BLACK},
+            axis_config={"color":LIGHT_GRAY},
             background_line_style={"stroke_opacity": 0}
         ).move_to(RIGHT*3)
         self.play(Create(title), Create(grid))
-        tex_f = MathTex(r"optimizing \ f(x,y) = x + y \\ \nabla f(x,y) = \begin{bmatrix} \partial f / \partial x \\ \partial f / \partial y \end{bmatrix} = \begin{bmatrix} 1 \\ 1 \end{bmatrix}").move_to(LEFT*4+UP)
-        tex_g = MathTex(r"given \ g(x,y) = x^2 + y^2 = 1 \\ \nabla g(x,y) = \begin{bmatrix} \partial g / \partial x \\ \partial g / \partial y \end{bmatrix} = \begin{bmatrix} 2x \\ 2y \end{bmatrix}").next_to(tex_f, DOWN)
-        tex_s = MathTex(r"requires \ \nabla f(x,y) = \lambda \nabla g(x,y)").next_to(tex_g, DOWN)
+        tex_f = MathTex(r"\text{optimizing} \ f(x,y) = x + y \\ \nabla f(x,y) = \begin{bmatrix} \partial f / \partial x \\ \partial f / \partial y \end{bmatrix} = \begin{bmatrix} 1 \\ 1 \end{bmatrix}").move_to(LEFT*4+UP)
+        tex_g = MathTex(r"\text{given} \ g(x,y) = x^2 + y^2 = 1 \\ \nabla g(x,y) = \begin{bmatrix} \partial g / \partial x \\ \partial g / \partial y \end{bmatrix} = \begin{bmatrix} 2x \\ 2y \end{bmatrix}").next_to(tex_f, DOWN)
+        tex_s = MathTex(r"\text{requires} \ \nabla f(x,y) = \lambda \nabla g(x,y)").next_to(tex_g, DOWN)
         tex_f.set_color(CS[0])
         tex_g.set_color(CS[1])
         tex_s.set_color(CS[-1])
@@ -409,9 +403,9 @@ class S(Scene):
         for i in range(-4, 5):
             for j in range(-4, 5):
                 if (i+j)%2: continue
-                arrows_f.append(Arrow(start=grid.c2p(i,j), end=grid.c2p(i+1,j+1), color=CS[0], stroke_opacity=0.5, stroke_width=4, tip_length=0.2))
-                arrows_f[-1].get_tip().set_opacity(0.5)
-        circle_g = Circle(radius=1, color=CS[1], stroke_opacity=0.5).move_to(grid.c2p(0,0))
+                arrows_f.append(Arrow(start=grid.c2p(i,j), end=grid.c2p(i+1,j+1), color=CS[0], stroke_opacity=0.625, stroke_width=4, tip_length=0.2))
+                arrows_f[-1].get_tip().set_opacity(0.625)
+        circle_g = Circle(radius=1, color=CS[1], stroke_opacity=0.625).move_to(grid.c2p(0,0))
         arrows_g = []
         import math
         theta = 0
